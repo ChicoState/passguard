fb7c00b48c11d3ebd81b90db91e6bbdca4512fb7
Paolo Gonzalez <paoloag133@gmail.com>
2025-03-05 05:09:22 -0800

    added functionality to upload and retrieve username/password

---
+++ passguard_app/lib/services/upload_retrieve.dart
@@ -0,0 +1,28 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+
+Future<void> uploadPass(String encUsername, String encPass, String hostName) async {
+  String uid = (FirebaseAuth.instance.currentUser?.uid ?? 'nullUser');
+  if (uid != 'nullUser'){
+    FirebaseFirestore.instance.collection("users").doc(uid).collection("accounts").doc(hostName).set({
+      'username': encUsername,
+      'password': encPass,      
+    });
+  }
+  
+}
+
+Future<MapEntry<String, String>> retrievePass(String hostName) async {
+  String uid = (FirebaseAuth.instance.currentUser?.uid ?? 'nullUser');
+  if (uid != 'nullUser'){
+    DocumentSnapshot docSnap = await FirebaseFirestore.instance.collection("users").doc(uid).collection("accounts").doc(hostName).get();
+    if (docSnap.exists){
+      Map<String, dynamic>? data = docSnap.data() as Map<String, dynamic>?;
+      if(data != null){
+        return MapEntry(data['username'], data['password']);
+      }
+    }
+  }
+  return MapEntry('not found', 'not found');
+}
