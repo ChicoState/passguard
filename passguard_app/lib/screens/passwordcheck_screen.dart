45972da8174c689c856698a5a0a25941ee2c391e
Cooper Howard <cooper.j.pie@gmail.com>
2025-03-07 15:02:35 -0800

    API implimentation

--- passguard_app/lib/screens/passwordcheck_screen.dart
+++ passguard_app/lib/screens/passwordcheck_screen.dart
@@ -1,15 +1,39 @@
 import 'package:flutter/material.dart';
+import 'package:passguard_app/passwordchecker.dart';
 
-class PasswordCheckScreen extends StatelessWidget {
-  const PasswordCheckScreen({super.key});
 
+TextEditingController mycontroller = TextEditingController();
+bool pwned = false;
+class PasswordCheckScreen extends StatefulWidget {
+  PasswordCheckScreen({super.key});
+
+  @override
+  _PasswordCheckScreenState createState() => _PasswordCheckScreenState();
+}
+
+class _PasswordCheckScreenState extends State<PasswordCheckScreen> {
+void _check () async {
+  bool x;
+  x = await checkPasswordPwned(mycontroller.text);
+  setState(() {
+        pwned = x;
+      });
+}
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text('Check Password Leak')),
-      body: const Center(
-        child: Text(''),
+      body: Column(
+        children: [
+          Text('$pwned'),
+          const SizedBox(height:300),
+          TextField(
+            controller: mycontroller,
+          ),
+          FilledButton(onPressed: _check, child: Text('Check')),
+        ],
       ),
+      
     );
   }
 }
