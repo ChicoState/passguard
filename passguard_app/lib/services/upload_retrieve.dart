baa7e6c07781dc5e8503c8b248a4ab113d4e7386
mateoguzv <mguzmanvanegas@mail.csuchico.edu>
2025-04-07 12:28:14 -0700

    Deleted passwordchecker.dart and updated upload_retrieve.dart

--- passguard_app/lib/services/upload_retrieve.dart
+++ passguard_app/lib/services/upload_retrieve.dart
@@ -1,6 +1,6 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:flutter/material.dart';
+//import 'package:flutter/material.dart';
 
 Future<void> uploadPass(String encUsername, String encPass, String hostName, [String? email]) async {
   String uid = (FirebaseAuth.instance.currentUser?.uid ?? 'nullUser');
