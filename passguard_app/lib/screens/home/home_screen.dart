797fb308decc605fd68de038b3a946df6079012c
aphanachone <aphanachone@gmail.com>
2025-04-09 10:46:30 -0700

    Added Password Generator to Home Screen

--- passguard_app/lib/screens/home/home_screen.dart
+++ passguard_app/lib/screens/home/home_screen.dart
@@ -6,6 +6,7 @@ import 'widgets/stats_card_row.dart';
 import 'widgets/accounts_list.dart';
 import 'widgets/add_account_dialog.dart';
 import 'package:passguard_app/theme.dart';
+import 'widgets/passgen.dart';
 
 class HomeScreen extends StatefulWidget {
   final String userId;
@@ -28,23 +29,27 @@ class _HomeScreenState extends State<HomeScreen> {
             Expanded(
               flex: 5, // Takes up 3 parts of the screen width
               child: Padding(
-                padding: const EdgeInsets.all(kDefaultPadding),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    // 1) header
-                    DashboardHeader(userId: widget.userId),
+                  padding: const EdgeInsets.all(kDefaultPadding),
+                  child: SingleChildScrollView(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      // 1) header
+                      DashboardHeader(userId: widget.userId),
 
-                    // 2) stats row
-                    Padding(
-                      padding: const EdgeInsets.only(top: kDefaultPadding),
-                      child: StatsCardRow(userId: widget.userId),
-                    ),
-                  ],
+                      // 2) stats row
+                      Padding(
+                        padding: const EdgeInsets.only(top: kDefaultPadding),
+                        child: StatsCardRow(userId: widget.userId),
+                      ),
+
+                      const SizedBox(height: kDefaultPadding),
+                      const PassGen(),
+                    ],
+                  ),
                 ),
               ),
             ),
-
             // Right side (accounts list and floating button)
             Expanded(
               flex: 3,
@@ -104,7 +109,6 @@ class _HomeScreenState extends State<HomeScreen> {
      TextEditingController _emailController =
          TextEditingController(text: doc['email'] ?? '');
 
- 
      showDialog(
        context: context,
        builder: (context) {
@@ -195,96 +199,5 @@ class _HomeScreenState extends State<HomeScreen> {
          );
        },
      );
-   }
-    TextEditingController _hostNameController =
-        TextEditingController(text: doc.id);
-    TextEditingController _usernameController =
-        TextEditingController(text: doc['username']);
-    TextEditingController _passwordController =
-        TextEditingController(text: doc['password']);
-
-    showDialog(
-      context: context,
-      builder: (context) {
-        bool _obscurePassword = true;
-        return StatefulBuilder(
-          builder: (context, setState) {
-            return AlertDialog(
-              title: const Text('Edit Account'),
-              content: SingleChildScrollView(
-                child: Column(
-                  children: [
-                    TextField(
-                      controller: _hostNameController,
-                      decoration: const InputDecoration(labelText: 'Host Name'),
-                      readOnly: true,
-                    ),
-                    TextField(
-                      controller: _usernameController,
-                      decoration: const InputDecoration(
-                          labelText: 'Username (optional)'),
-                    ),
-                    TextField(
-                      controller: _passwordController,
-                      obscureText: _obscurePassword,
-                      decoration: InputDecoration(
-                        labelText: 'Password',
-                        suffixIcon: IconButton(
-                          icon: Icon(_obscurePassword
-                              ? Icons.visibility_off
-                              : Icons.visibility),
-                          onPressed: () {
-                            setState(() {
-                              _obscurePassword = !_obscurePassword;
-                            });
-                          },
-                        ),
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-              actions: [
-                TextButton(
-                  onPressed: () {
-                    Navigator.of(context).pop();
-                  },
-                  child: const Text('Cancel'),
-                ),
-                // Delete button in red
-                TextButton(
-                  onPressed: () async {
-                    await FirebaseFirestore.instance
-                        .collection('users')
-                        .doc(widget.userId)
-                        .collection('accounts')
-                        .doc(doc.id)
-                        .delete();
-                    Navigator.of(context).pop();
-                  },
-                  child: const Text(
-                    'Delete',
-                    style: TextStyle(color: Colors.red),
-                  ),
-                ),
-                ElevatedButton(
-                  onPressed: () async {
-                    String hostName = _hostNameController.text.trim();
-                    String username = _usernameController.text.trim();
-                    String password = _passwordController.text.trim();
-                    if (hostName.isNotEmpty && password.isNotEmpty) {
-                      // Update via the service function.
-                      await uploadPass(username, password, hostName);
-                      Navigator.of(context).pop();
-                    }
-                  },
-                  child: const Text('Update'),
-                ),
-              ],
-            );
-          },
-        );
-      },
-    );
+    }
   }
-
