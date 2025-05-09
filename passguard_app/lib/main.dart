23665c6b5eaa96c08ca34af9a418125a91f4f397
Malene-Kavanagh <makavanagh@csuchico.edu>
2025-05-09 16:33:23 -0700

    I am getting a bunch of errors. Trouble shooting now

--- passguard_app/lib/main.dart
+++ passguard_app/lib/main.dart
@@ -4,7 +4,6 @@ import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
 import 'screens/introduction_screen.dart';
 //attempt extension
-import 'package:chrome_extension/extension.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
